68b228bfcd009517a03b3ece474d20eb7687c5df
AlekOmOm <142972647+AlekOmOm@users.noreply.github.com>
2023-09-08 20:50:14 +0200

    1

--- src/Service/Opskrift.java
+++ src/Service/Opskrift.java
@@ -11,6 +11,7 @@ public class Opskrift {
     ArrayList<Ingredienser> opskrift = new ArrayList<Ingredienser>();
 
     public void createIngredienser() {
+        // ingredienser objekter skabes og sættes med originalt værdi
         // ingrediens mængder og kcal er beregnet efter 'originalen' 4 personer, de beregnes dermed efter *(nyt antal/originalt(4))
         Ingredienser mælk = new Ingredienser("mælk", 1, "liter", 600);
         Ingredienser mel = new Ingredienser("mel", 500, "gram", 1500);
@@ -18,6 +19,7 @@ public class Opskrift {
         Ingredienser sukker = new Ingredienser("sukker", 1, "spsk", 100);
         Ingredienser smør = new Ingredienser("smør", 50, "gram", 400);
         Ingredienser bagepulver = new Ingredienser("bagepulver", 1, "tsk", 0);
+        // ingrediens objekterne tilføjes til opskrift, hvilket er et array objekt, så tilføjes objekt.add()
         opskrift.add(mælk);
         opskrift.add(mel);
         opskrift.add(æg);
@@ -25,11 +27,13 @@ public class Opskrift {
         opskrift.add(smør);
         opskrift.add(bagepulver);
         beregner.bestemAntal();
+        // ingredienser i opskrift-array gåes igennem og beregnes korrekt mængde og kcal.
         for (Ingredienser ingrediens : opskrift) {
             beregner.beregnMængde(ingrediens);
             beregner.beregnKcal(ingrediens);
         }
 
+        // opskrift skabt, så metoden printer opskriften
         System.out.println(opskrift);
 
 
